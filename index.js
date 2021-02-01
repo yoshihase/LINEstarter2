a309dfc0fc37a55d62aaa67cbf93747e293863ef
Yoshi <66835593+yoshihase@users.noreply.github.com>
2021-02-01 15:53:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ $(function () {
         // var tool = $('input[name="tool"]').val();
         
         //var msg = `${key}\n朝礼/終礼不参加の理由：${reasons}\n返信期限：${date}\n使用ツール：${tool}`;
-        var msg = `${key}\n予約日：${reasons}`;
+        var msg = `${key}\n予約日：${date}`;
         sendText(msg);
 
         return false;
